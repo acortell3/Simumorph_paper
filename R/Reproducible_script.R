5b6ae82d4e91209f4bd1cf4985e635ebce5252b0
acortell3 <alfredo.cortell.nicolau@gmail.com>
2025-12-02 16:51:35 +0100

    added simulations

--- R/Reproducible_script.R
+++ R/Reproducible_script.R
@@ -31,31 +31,50 @@ dev.off()
 ###################################################################################
 
 ###################################################################################
-## Figure 4. Case study 1
+## Figure 4a. Case study 1
 set.seed(seed)
 
-AtoA <- simumorph(x = amp_pha_cov, m.space = amp_pha_mat, init = 1, target = 1, method = "AtoA", sim = sims, npts = npts, only.shapes = F, a = 0.2, e = 0.05, f = 100)
+AtoAa <- simumorph(x = amp_pha_cov, m.space = amp_pha_mat, init = 1, target = 1, method = "AtoA", sim = sims, npts = npts, only.shapes = F, a = 0.2, e = 0.05, f = 100)
 
 ## Plot
-png("../Figures/Fig_4.png", res = 50, height = 1500, width = 1500)
+png("../Figures/Fig_4a.png", res = 50, height = 1500, width = 1500)
 layout(mat)
 for (i in 1:sims){
-	plot(AtoA$Shapes[[i]], type = "l", lwd = 1.5, cex.main = 2, xlab = "", ylab = "", bty = "n", main = paste0("t = ",i))
-	polygon(AtoA$Shapes[[i]], col = adjustcolor("seagreen", alpha = 0.85))
+	plot(AtoAa$Shapes[[i]], type = "l", lwd = 1.5, cex.main = 2, xlab = "", ylab = "", bty = "n", main = paste0("t = ",i))
+	polygon(AtoAa$Shapes[[i]], col = adjustcolor("seagreen", alpha = 0.85))
 }
-plot(unlist(AtoA$P.distances), type = "l", lwd = 3, cex.main = 2, cex.lab = 2.2, mgp = c(1.8,0.7,0), col = "seagreen3", main = "Procrustes distances", xlab = "Time", ylab = "Dist", bty = "n")
+plot(unlist(AtoAa$P.distances), type = "l", lwd = 3, cex.main = 2, cex.lab = 2.2, mgp = c(1.8,0.7,0), col = "seagreen3", main = "Procrustes distances", xlab = "Time", ylab = "Dist", bty = "n")
 dev.off()
 ###################################################################################
 
+###################################################################################
+## Figure 4b. Case study 1 (dynamic)
+set.seed(seed)
+
+dyn_e <- data.frame("time" = c(1,10,40,70,85,95),
+		    "e" = c(0.02,0.03,0.25,0.2,0.15,0.1))
+
+AtoAb <- simumorph(x = amp_pha_cov, m.space = amp_pha_mat, init = 1, target = 1, method = "AtoA", sim = sims, npts = npts, only.shapes = F, a = 0.2, f = 100, dynamic_e = dyn_e)
+
+## Plot
+png("../Figures/SI_figures/Fig_4b.png", res = 50, height = 1500, width = 1500)
+layout(mat)
+for (i in 1:sims){
+	plot(AtoAb$Shapes[[i]], type = "l", lwd = 1.5, cex.main = 2, xlab = "", ylab = "", bty = "n", main = paste0("t = ",i))
+	polygon(AtoAb$Shapes[[i]], col = adjustcolor("seagreen", alpha = 0.85))
+}
+plot(unlist(AtoAb$P.distances), type = "l", lwd = 3, cex.main = 2, cex.lab = 2.2, mgp = c(1.8,0.7,0), col = "seagreen3", main = "Procrustes distances", xlab = "Time", ylab = "Dist", bty = "n")
+dev.off()
+###################################################################################
 
 ###################################################################################
 ## Figure 5. Case study 2a
 set.seed(seed)
 
-AtoBa <- simumorph(x = amp_pha_cov, m.space = amp_pha_mat, init = 1, target = which(rownames(amp_pha_mat)=="G18_m_G18"), method = "AtoB", sim = sims, npts = npts, only.shapes = F, a = 0.5, e = 0.5, f = 100, max.attempts = 500)
+AtoBa <- simumorph(x = amp_pha_cov, m.space = amp_pha_mat, init = 1, target = which(rownames(amp_pha_mat)=="G18_m_G18"), method = "AtoB", sim = sims, npts = npts, only.shapes = F, a = 0.5, e = 0.5, f = 100, max.attempts = 500, speedAtoB = 0)
 
 ## Plot
-png("../Figures/Fig_5.png", res = 50, height = 1500, width = 1500)
+png("../Figures/Fig_5a.png", res = 50, height = 1500, width = 1500)
 layout(mat)
 for (i in 1:sims){
 	plot(AtoBa$Shapes[[i]], type = "l", lwd = 1.5, cex.main = 2, xlab = "", ylab = "", bty = "n", main = paste0("t = ",i))
@@ -65,6 +84,41 @@ plot(unlist(AtoBa$P.distances), type = "l", lwd = 3, cex.main = 2, cex.lab = 2.2
 dev.off()
 ###################################################################################
 
+###################################################################################
+## Figure 5. Case study 2b (higher dist)
+set.seed(seed)
+
+AtoBb <- simumorph(x = amp_pha_cov, m.space = amp_pha_mat, init = 1, target = which(rownames(amp_pha_mat)=="G18_m_G18"), method = "AtoB", sim = 500, npts = npts, only.shapes = F, a = 0.5, e = 0.5, f = 100, max.attempts = 500, speedAtoB = 0.0035)
+
+samp <- seq(1,500,5)
+
+## Plot
+png("../Figures/SI_figures/Fig_5b.png", res = 50, height = 1500, width = 1500)
+layout(mat)
+for (i in 1:sims){
+	plot(AtoBb$Shapes[[samp[i]]], type = "l", lwd = 1.5, cex.main = 2, xlab = "", ylab = "", bty = "n", main = paste0("t = ",samp[i]))
+	polygon(AtoBb$Shapes[[samp[i]]], col = adjustcolor("sienna4", alpha = 0.85))
+}
+plot(unlist(AtoBb$P.distances), type = "l", lwd = 3, cex.main = 2, cex.lab = 2.2, mgp = c(1.8,0.7,0), col = "sienna4", main = "Procrustes distances", xlab = "Time", ylab = "Dist", bty = "n")
+dev.off()
+###################################################################################
+
+###################################################################################
+## Figure 5. Case study 2c (lower dist)
+set.seed(seed)
+
+AtoBc <- simumorph(x = amp_pha_cov, m.space = amp_pha_mat, init = 1, target = which(rownames(amp_pha_mat)=="G18_m_G18"), method = "AtoB", sim = sims, npts = npts, only.shapes = F, a = 0.5, e = 0.5, f = 100, max.attempts = 500, speedAtoB = -0.002)
+
+## Plot
+png("../Figures/SI_figures/Fig_5c.png", res = 50, height = 1500, width = 1500)
+layout(mat)
+for (i in 1:sims){
+	plot(AtoBc$Shapes[[i]], type = "l", lwd = 1.5, cex.main = 2, xlab = "", ylab = "", bty = "n", main = paste0("t = ",i))
+	polygon(AtoBc$Shapes[[i]], col = adjustcolor("sienna4", alpha = 0.85))
+}
+plot(unlist(AtoBc$P.distances), type = "l", lwd = 3, cex.main = 2, cex.lab = 2.2, mgp = c(1.8,0.7,0), col = "sienna4", main = "Procrustes distances", xlab = "Time", ylab = "Dist", bty = "n")
+dev.off()
+###################################################################################
 ###################################################################################
 ## Figure 6. Case study 3
 set.seed(seed)
